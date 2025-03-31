b7d2101c123a4e7bfc398f88bed5e4575f52918b
KurtRavn <158466708+TrylleLeif@users.noreply.github.com>
2025-03-31 18:46:05 +0200

    sidste

--- task_3/__init__.py
+++ task_3/__init__.py
@@ -2,9 +2,6 @@ from utils.data import get_fixed_data
 from utils.WindProcess import wind_model
 from utils.PriceProcess import price_model
 from task_3.helper_functions import (
-    visualize_policy_decisions,
-    calculate_feature_importance,
-    generate_value_function_heatmap,
-    compare_adp_with_stochastic_programming,
-    analyze_decision_patterns
-)
+    check_feasibility,
+    sample_representative_states,
+    add_critical_region_samples)
