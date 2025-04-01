c07842e702cc8e95da872438f4fcd618967d42e2
KurtRavn <158466708+TrylleLeif@users.noreply.github.com>
2025-04-01 11:43:06 +0200

    hæhæ

--- task_3/__init__.py
+++
@@ -1,7 +0,0 @@
-from utils.data import get_fixed_data
-from utils.WindProcess import wind_model
-from utils.PriceProcess import price_model
-from task_3.helper_functions import (
-    check_feasibility,
-    sample_representative_states,
-    add_critical_region_samples)
